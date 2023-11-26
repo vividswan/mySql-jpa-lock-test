745e3faf83a6fd1530833f73b935b8015bd109a0
vividswan <vividswan@naver.com>
2023-11-26 20:41:30 +0900

    project init

---
+++ src/main/java/com/vividswan/locktest/LocktestApplication.java
@@ -0,0 +1,13 @@
+package com.vividswan.locktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocktestApplication.class, args);
+	}
+
+}

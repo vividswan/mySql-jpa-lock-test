e1cd21a78cd1a361220fb7df00bf4957b8596e0b
vividswan <vividswan@naver.com>
2023-12-01 20:42:15 +0900

    test: Synchronized 되지 않은 경쟁 상태 예시 테스트 코드 작성

--- src/test/java/com/vividswan/locktest/service/ProductServiceTest.java
+++ src/test/java/com/vividswan/locktest/service/ProductServiceTest.java
@@ -2,6 +2,10 @@ package com.vividswan.locktest.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -43,4 +47,31 @@ class ProductServiceTest {
 		// then
 		assertEquals(99, product.getQuantity());
 	}
+
+	@Test
+	@DisplayName("동시에 100개가 요청 되는 코드_in_race_condition")
+	public void 동시에_100개의_요청() throws InterruptedException {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(20);
+		CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					productService.decreaseStock(1L, 1L);
+				} finally {
+					countDownLatch.countDown();
+				}
+			});
+		}
+
+		countDownLatch.await();
+		productService.decreaseStock(1L, 1L);
+		Product product = productRepository.findById(1L)
+			.orElseThrow(() -> new RuntimeException("존재하지 않는 상품"));
+
+		// race condition에 의해 not equal
+		assertNotEquals(0, product.getQuantity());
+	}
+
 }

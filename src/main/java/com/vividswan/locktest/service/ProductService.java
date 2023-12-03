dd6df6d01b4592c514adf0193ad406c97fe882b7
vividswan <vividswan@naver.com>
2023-12-03 19:11:01 +0900

    chore: synchronized 관련 주석 추가

--- src/main/java/com/vividswan/locktest/service/ProductService.java
+++ src/main/java/com/vividswan/locktest/service/ProductService.java
@@ -27,6 +27,7 @@ public class ProductService {
 		productById.get().decrease(quantity);
 	}
 
+	// 프록시 패턴에 의해 관련 메소드를 래핑한 클래스에서 트랜잭션을 끝내서 DB에 저장하기 전 다른 쓰레드가 synchronized 메서드에 접근 가능하기 때문에 @Transaction이 있으면 제대로 동작하지 않음
 	public synchronized void synchronizedDecreaseStock(Long id, Long quantity) {
 		Optional<Product> productById = productRepository.findById(id);
 
